01d713593960573df205447319489f61fd73d7d4
David Gómez <ldavid.github@gmail.com>
2016-08-15 18:43:02 +0200

    added Ajax request and github helper

--- github-battle/app/containers/ConfirmBattleContainer.js
+++ github-battle/app/containers/ConfirmBattleContainer.js
@@ -3,6 +3,7 @@
  */
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle')
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
@@ -12,28 +13,25 @@ var ConfirmBattleContainer = React.createClass({
         console.log('getInitialState');
         return {
             isLoading: true,
-            playerInfo: [],
+            playersInfo: [],
         }
-    },
-    componentWillMount: function () {
-      console.log('componentWillMount');
     },
     componentDidMount: function () {
         console.log('componentDidMount');
         var query = this.props.location.query;
-        // Fetch info from github then update state
-    },
-    componentWillReceiveProps: function () {
-        console.log('componentWillReceiveProps');
-    },
-    componentWillUnmount: function () {
-        console.log('componentWillUnmount');
+        githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+            .then(function (players) {
+                this.setState({
+                    isLoading: false,
+                    playersInfo: [players[0], players[1]]
+                })
+            }.bind(this));
     },
     render: function () {
         return (
             <ConfirmBattle
                 isLoading={this.state.isLoading}
-                playersInfo={this.state.playerInfo}
+                playersInfo={this.state.playersInfo}
             />
         );
     }
